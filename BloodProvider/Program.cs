0fec0c9e34de3c49c3af94fafccfe005c4ef68ed
Nitesh Jaiswal <nitesh.singh1423.nl@gmail.com>
2025-05-04 02:09:32 +0545

    working login and register

--- BloodProvider/Program.cs
+++ BloodProvider/Program.cs
@@ -9,7 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+        sqlOptions =>
+        {
+            sqlOptions.EnableRetryOnFailure();
+        }));
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options =>
 {
